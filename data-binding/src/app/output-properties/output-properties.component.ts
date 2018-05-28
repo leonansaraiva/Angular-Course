eedf741df0be995639c8942b9b270980c5d24f11
leonansaraiva <leonandasilvasaraiva@gmail.com>
2018-05-27 23:35:56 -0300

    Diretivas

--- data-binding/src/app/output-properties/output-properties.component.ts
+++ data-binding/src/app/output-properties/output-properties.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input ,EventEmitter, Output } from '@angular/core';
+import { Component, OnInit,Input ,EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -11,12 +11,17 @@ export class OutputPropertiesComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter();
 
-  incrementa(){
+
+  @ViewChild('campoInput') campoValorInput: ElementRef;
+
+  incrementa() {
+    console.log(this.campoValorInput.nativeElement.value);
     this.valor++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
-  decrementa(){
+  decrementa() {
+    console.log(this.campoValorInput.nativeElement.value);
     this.valor--;
     this.mudouValor.emit({novoValor: this.valor});
   }
